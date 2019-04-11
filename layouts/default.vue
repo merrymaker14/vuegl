daf2e89bf3911e25b105fdcc34fcf9aab8862ec6
tmusharapov <tmusharapov@TMUSHARAPOV>
2019-04-11 16:01:02 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="layout">
+    <div class="layout__animation">
+      <Animation />
+    </div>
+    <div class="layout__content">
+      <nuxt />
+    </div>
+    <div class="layout__navigation">
+      <Navigation />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navigation from '~/components/Navigation';
+  import Animation from '~/components/Animation';
+
+  export default {
+    components: {
+      Navigation,
+      Animation
+    }
+  }
+</script>
+
+<style>
+  body {
+    background: #000;
+    font-family: helvetica, sans-serif;
+  }
+
+  .layout__navigation {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+
+  .layout__content {
+    font-size: 100px;
+    font-weight: bold;
+    color: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+  }
+
+  .slide-enter-active {
+    transition: all .3s ease-in-out;
+  }
+
+  .slide-leave-active {
+    transition: all .3s ease-in-out;
+  }
+
+  .slide-enter {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  .slide-enter-to {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+
+  .slide-leave {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+  .slide-leave-to {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+import pkg from './package'
+
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/vuegl/'
+  }
+} : {}
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  ...routerBase,
+
+  router: {
+     middleware: 'pages',
+     //base: '/examples/vuegl/'
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
